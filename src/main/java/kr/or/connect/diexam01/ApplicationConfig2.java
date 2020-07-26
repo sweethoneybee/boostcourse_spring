abf6604e4ef795575dcebbcc4e66ac813ab70d60
jsjphone8 <jsjphone8@gmail.com>
2020-07-26 21:00:10 +0900

    Java config를 이용한 설정

---
+++ src/main/java/kr/or/connect/diexam01/ApplicationConfig2.java
@@ -0,0 +1,10 @@
+package kr.or.connect.diexam01;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("kr.or.connect.diexam01") // 패키지명 안에 있는 것들 중에 스캔해와 
+public class ApplicationConfig2 {
+	
+}
